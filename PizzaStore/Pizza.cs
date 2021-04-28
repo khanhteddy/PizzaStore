702efeb0ac1c56ada694de9db0772fff8f873e77
Cong Khanh <khanhnguyen147369@gmail.com>
2021-04-28 15:10:18 +0700

    Add project files.

---
+++ PizzaStore/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore
+{
+    public class Pizza
+    {
+        protected string name;
+        protected string dough;
+        protected string sauce;
+        protected List<string> toppings;
+
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing " + name);
+            Console.WriteLine("Tossing dough ...");
+            Console.WriteLine("Adding sauce ...");
+            Console.WriteLine("Adding toppings: ");
+            toppings?.ForEach(
+                (topping) => Console.WriteLine("\t" + topping));
+        }
+        public void Bake()
+        {
+            Console.WriteLine("Baking" + name + "30 mins");
+
+        }
+        public void Cut()
+        {
+            Console.WriteLine("Cutting" + name);
+        }
+        public void Box()
+        {
+            Console.WriteLine("Boxing " + name);
+        }
+    }
+}
---
+++ PizzaStore/PizzaStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore
+{
+    public class PizzaStore
+    {
+        public void OederPizza()
+        {
+            
+                var pizza = new Pizza();
+                pizza.Prepare();
+                pizza.Bake();
+                pizza.Cut();
+                pizza.Box();
+
+            
+        }
+    }
+}
